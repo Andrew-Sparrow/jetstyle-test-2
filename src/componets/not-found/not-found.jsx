fbc99a8b9b27b0b25ed74d83a0fe92023fd4aba4
Andrew Pechers <8911102@gmail.com>
2022-06-05 19:43:37 +0600

    добавлены стили для not-found

--- src/componets/not-found/not-found.jsx
+++ src/componets/not-found/not-found.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import withLayout from '../hocs/with-layout';
+import { Link } from 'react-router-dom';
+import { AppRoute } from '../../const';
 
 function NotFound() {
   return (
     <div style={{margin: 'auto', width: '200px'}}>
       <h1 style={{textAlign: 'center'}}>404</h1>
-      <h2>Page Not Found</h2>
+      <h2 className="not-found" >Page Not Found</h2>
+      <Link className="not-found__link" to={AppRoute.MAIN}>Return to main page</Link>
     </div>
   );
 }
